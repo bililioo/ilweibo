754fc546cbb21b3a8751589fe7f722c3775da7c5
BunChan <schumacher23@qq.com>
2018-05-15 15:49:10 +0800

    ilweibo.sql

--- ilweibo.sql
+++ ilweibo.sql
@@ -1,19 +1,46 @@
--- create by Ben.
--- 如果存在先删除库
-drop database if exists ilweibo;
--- 建库
-create database ilweibo;
+/*
+ Navicat Premium Data Transfer
 
-use ilweibo;
+ Source Server         : RootSQL
+ Source Server Type    : MySQL
+ Source Server Version : 50721
+ Source Host           : localhost:3306
+ Source Schema         : ilweibo
 
-grant select, insert, update, delete on ilweibo.* to 'www-data'@'localhost' identified by 'www-data';
+ Target Server Type    : MySQL
+ Target Server Version : 50721
+ File Encoding         : 65001
 
--- 建表
-create table comments (
-    `pic` text(300),
-    `text` text(300),
-    `name` text(30),
-	`id` VARCHAR(512) not null,
-   	primary key (`id`)
-) engine=innodb default charset=utf8;
+ Date: 15/05/2018 15:11:41
+*/
 
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for comments
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `pic` text,
+  `text` text,
+  `name` tinytext,
+  `r_uid` text,
+  `id` varchar(512) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for weibo
+-- ----------------------------
+DROP TABLE IF EXISTS `weibo`;
+CREATE TABLE `weibo` (
+  `pic` text,
+  `text` text,
+  `name` tinytext,
+  `r_uid` text,
+  `id` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
