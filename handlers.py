065417b32b632dda99fc26f2f8e071c87d43dab5
BunChan <schumacher23@qq.com>
2018-06-02 16:05:31 +0800

    修改接口

--- handlers.py
+++ handlers.py
@@ -33,7 +33,7 @@ async def api_weibo(*, pageIndex):
         raise APIValueError('pageIndex')
 
     pageIndex = int(pageIndex) * 20
-    weibo_arr = await models.weibo.findAll(limit=(pageIndex, 20), orderBy='time desc')
+    weibo_arr = await models.weibo.findAll(limit=(pageIndex, 20), orderBy='time desc', where='report = 0')
     for weibo in weibo_arr:
         weibo.time = str(weibo.time)
     return {'data': weibo_arr, 'count': len(weibo_arr)}
