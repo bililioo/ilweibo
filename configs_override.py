aff6b0af2c5c8b62dd77069f90f2188ea5c931f2
BunChan <schumacher23@qq.com>
2018-06-05 18:30:19 +0800

    fix bug

--- configs_override.py
+++ configs_override.py
@@ -5,16 +5,16 @@ __author__ = 'Ben'
 
 configs = {
     'apis':{
-        # 'host': '172.16.16.16',
-        'host': '10.1.52.163', 
+        'host': '172.16.16.16',
+        # 'host': '10.1.52.163', 
         # 'host': '192.168.1.103',
     },
-    'db': {
-        'host': '111.230.252.244',
-        'port': 3306,
-        'user': 'root',
-        'password': 'Bunchan16',
-        'database': 'ilweibo'
-    },
+    # 'db': {
+    #     'host': '111.230.252.244',
+    #     'port': 3306,
+    #     'user': 'root',
+    #     'password': 'Bunchan16',
+    #     'database': 'ilweibo'
+    # },
 }
 
